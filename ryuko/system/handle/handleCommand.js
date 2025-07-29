a47b837d1d4410bc0b0cd738971ad2fed8f51426
HASAN-CEO-47 <shhabdur@gmail.com>
2025-07-29 20:50:08 +0600

    Update handleCommand.js

--- ryuko/system/handle/handleCommand.js
+++ ryuko/system/handle/handleCommand.js
@@ -1,7 +1,7 @@
 module.exports = function({ api, models, Users, Threads, Currencies }) {
   const stringSimilarity = require('string-similarity'),
     escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"),
-    logger = require("../../catalogs/ryukoc.js");
+    logger = require("../../catalogs/Jubayerc.js");
   const axios = require('axios')
   const moment = require("moment-timezone");
   return async function({ event }) {
